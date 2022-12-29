5d6335a20a9386c7071a70004c91dc36650adf04
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2022-12-29 13:50:30 +0900

    Feat: 초기 라우팅 세팅

---
+++ src/pages/Tech/Tech.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Tech = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      적용 기술 페이지 입니다.
+    </div>
+  );
+};
+
+export default Tech;
