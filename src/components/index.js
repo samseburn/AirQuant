8f8b869e49e469ec8a2d9f5d304b97e5a80f1419
hwayoung <hyriver37@gamil.com>
2023-01-02 16:15:40 +0900

    Feat: 고객 문의 페이지 컴포넌트 구조 설계

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,7 @@ export { Footer } from './common/Footer';
 
 // module
 export { Card } from './module/Card';
+export { Faq } from './module/Faq';
+export { AnswerBox } from './module/AnswerBox';
+export { QuestionBox } from './module/QuestionBox';
+export { Box } from './module/Box';
---
+++ src/components/module/QuestionBox/QuestionBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuestionBox = () => {
+  return (
+    <div>
+      <div className="title">명의변경을 하려면 어떻게 하나요?</div>
+      <div className="icon">@</div>
+    </div>
+  );
+};
+
+export default QuestionBox;
