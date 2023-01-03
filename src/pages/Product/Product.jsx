c0f76363a3d45296a88ca2ef30c7370f119e3541
seoyoon <tkddnjs1406@icloud.com>
2023-01-03 14:26:29 +0900

    Docs: Product 문서 내용 제거 및 수정

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -14,10 +14,6 @@ const Product = () => {
       }}
     >
 
-      <ProductPiquantExplanation />
-      <ProductMainImg />
-      {/* <InformationBox /> */}
-      <ProductAirTable />
     </div>
   );
 };
