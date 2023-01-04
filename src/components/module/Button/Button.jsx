19ba44b2dc2fb8a0cfc94c092d574a7cd827368a
eunye <dms8945@gmail.com>
2023-01-04 12:15:30 +0900

    Feat: Button 컴포넌트 페이지 이동 기능 추가

--- src/components/module/Button/Button.jsx
+++ src/components/module/Button/Button.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import '../../../styles/Button.scss';
 
 const Button = (props) => {
-  return <div className="Button">{props.text}</div>;
+  return (
+    <div
+      className="Button"
+      onClick={() => window.open(`${props.url}`, '_blank')}
+    >
+      {props.text}
+    </div>
+  );
 };
 
 export default Button;
