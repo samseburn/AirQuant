e3f00e474b75085ed7a829d945b21700f25d2fbb
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2023-01-06 09:29:24 +0900

    Add: 이미지 assets 추가

---
+++ src/images/index.js
@@ -0,0 +1,51 @@
+// img 사용 방식
+// src에 변수로 삽입
+// <img src={airquant_01} alt="" />
+
+// Product
+export { default as airquant_01 } from './Product/airquant_01.jpeg';
+export { default as airquant_02 } from './Product/airquant_02.jpeg';
+export { default as airquant_03 } from './Product/airquant_03.jpeg';
+export { default as airquant_04 } from './Product/airquant_04.jpeg';
+export { default as airquant_05 } from './Product/airquant_05.jpeg';
+export { default as airquant_06 } from './Product/airquant_06.jpeg';
+export { default as airquant_07 } from './Product/airquant_07.jpeg';
+export { default as airquant_08 } from './Product/airquant_08.jpeg';
+export { default as airquant_09 } from './Product/airquant_09.png';
+export { default as airquant_10 } from './Product/airquant_10.png';
+export { default as airquant_11 } from './Product/airquant_11.png';
+export { default as table_logo } from './Product/table_logo.png';
+
+// Tech
+export { default as convenience_01 } from './Tech/convenience_01.png';
+export { default as convenience_02 } from './Tech/convenience_02.png';
+export { default as convenience_03 } from './Tech/convenience_03.png';
+export { default as convenience_04 } from './Tech/convenience_04.png';
+export { default as convenience_05 } from './Tech/convenience_05.png';
+export { default as convenience_06 } from './Tech/convenience_06.png';
+export { default as convenience_07 } from './Tech/convenience_07.jpg';
+export { default as convenience_08 } from './Tech/convenience_08.png';
+export { default as convenience_09 } from './Tech/convenience_09.png';
+export { default as explanation_phone } from './Tech/explanation_phone.png';
+export { default as explanation_weather } from './Tech/explanation_weather.png';
+export { default as logo_01 } from './Tech/logo_01.jpg';
+export { default as logo_02 } from './Tech/logo_02.jpg';
+export { default as logo_03 } from './Tech/logo_03.jpg';
+export { default as logo_04 } from './Tech/logo_04.jpg';
+export { default as logo_05 } from './Tech/logo_05.jpg';
+export { default as patent_01 } from './Tech/patent_01.png';
+export { default as patent_02 } from './Tech/patent_02.png';
+export { default as patent_03 } from './Tech/patent_03.png';
+export { default as patent_04 } from './Tech/patent_04.png';
+export { default as patent_05 } from './Tech/patent_05.png';
+
+// Company
+export { default as company_icon_01 } from './Company/company_icon_01.png';
+export { default as company_icon_02 } from './Company/company_icon_02.png';
+export { default as company_icon_03 } from './Company/company_icon_01.png';
+export { default as company_main } from './Company/company_main.png';
+export { default as company_product } from './Company/company_product.png';
+
+// Case
+
+// Contact
