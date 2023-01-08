22223c0a85fd08b74476e0f0ccecfe603549459b
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2023-01-08 20:34:24 +0900

    Design: 사이드바 스타일링

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -4,23 +4,30 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar lg-hidden">
-      <ol>
-        <li>
-          <Link to={'./'}>제품소개</Link>
-        </li>
-        <li>
-          <Link to={'./case'}>적용사례</Link>
-        </li>
-        <li>
-          <Link to={'./company'}>회사소개</Link>
-        </li>
-        <li>
-          <Link to={'./case'}>적용사례</Link>
-        </li>
-        <li>
-          <Link to={'./service'}>고객문의</Link>
-        </li>
-      </ol>
+      <div className="gnb">
+        <h1 className="logo">
+          <Link to={'./'}>AirQuant</Link>
+        </h1>
+        <div>
+          <ol>
+            <li>
+              <Link to={'./'}>제품소개</Link>
+            </li>
+            <li>
+              <Link to={'./tech'}>적용기술</Link>
+            </li>
+            <li>
+              <Link to={'./company'}>회사소개</Link>
+            </li>
+            <li>
+              <Link to={'./case'}>적용사례</Link>
+            </li>
+            <li>
+              <Link to={'./service'}>고객문의</Link>
+            </li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
