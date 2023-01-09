340ce09bb078dc63c36d61f400fab2ab56527be1
eunye <dms8945@gmail.com>
2023-01-09 14:59:54 +0900

    Feat: Button 컴포넌트 sendData 함수 추가

--- src/components/module/Button/Button.jsx
+++ src/components/module/Button/Button.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+
 const Button = (props) => {
+  // props[functionName(sendData,openWindow),url,text] 받고
+  const sendData = () => {
+    console.log(`== send data to firebase ==`);
+  };
+  const openWindow = () => {
+    return window.open(`${props.url}`, '_blank');
+  };
+
+  useEffect(() => {
+    sendData();
+    console.log(`안녕`);
+    openWindow();
+  }, [sendData, openWindow]);
+
   return (
-    <div
-      className="Button"
-      onClick={() => window.open(`${props.url}`, '_blank')}
-    >
+    <div className="Button" onClick={openWindow}>
       {props.text}
     </div>
   );
