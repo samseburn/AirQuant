7b79fe69f88b8659a0e47eb54e907d6a57b77a0d
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2023-01-11 21:36:28 +0900

    Fix: 메뉴 클릭 시 상단으로 이동

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,9 +1,11 @@
 import { Header, Footer, Navbar } from 'components';
 import { Outlet } from 'react-router-dom';
+import { ScrollToTop } from 'utils';
 
 const Root = ({ children }) => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
