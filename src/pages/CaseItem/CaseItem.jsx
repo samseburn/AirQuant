6f32a554fcfd597b46de61111beb16e9a040aba4
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2023-01-11 09:55:13 +0900

    Feat: 라우팅 구조 변경 및 적용 사례 데이터 전역 관리

--- src/pages/CaseItem/CaseItem.jsx
+++ src/pages/CaseItem/CaseItem.jsx
@@ -2,7 +2,12 @@ import { TableItem } from 'components';
 import React from 'react';
 
 const CaseItem = () => {
-  return <TableItem />;
+  return (
+    <div className="case">
+      <h1 className="case-title">적용 사례</h1>
+      <TableItem />
+    </div>
+  );
 };
 
 export default CaseItem;
