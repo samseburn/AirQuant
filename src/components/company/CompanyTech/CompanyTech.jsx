58a5c92cc0acaabddc0db72f0fe9dd7430b29713
eunye <dms8945@gmail.com>
2023-01-14 16:49:36 +0900

    Design: CompanyTech-Text 디자인 변경

--- src/components/company/CompanyTech/CompanyTech.jsx
+++ src/components/company/CompanyTech/CompanyTech.jsx
@@ -37,24 +37,42 @@ const CompanyTech = () => {
         </div>
         <span>을 사용합니다</span>
       </div>
-
       <div className="CompanyTech-Text">
-        <p>
-          <strong>측정 가능 물질 상태 : </strong>
-          고체, 액체, 기체
-        </p>
-        <p>
-          <strong>검출 한계 : </strong>
-          *100 nano mol / Liter{' '}
-          <span className="CompanyTech-Text-Small">
-            *100 nano mol/Liter는 소금물 100만 리터에 소금, 약 6g의 농도와
-            같습니다.
-          </span>
-        </p>
-        <p>
-          <strong>제품 타입 : </strong>
-          분광 분석 기반의 물질 성분 분석 장비 혹은 분광 분석 모듈
-        </p>
+        <div
+          className="CompanyTech-Text-Box"
+          data-aos="fade-up"
+          data-aos-delay="400"
+        >
+          <div className="CompanyTech-Text-Box-Title">
+            <strong>측정 가능 물질 상태</strong>
+          </div>
+          <div className="CompanyTech-Text-Box-Content"> 고체, 액체, 기체</div>
+        </div>
+        <div
+          className="CompanyTech-Text-Box"
+          data-aos="fade-up"
+          data-aos-delay="600"
+        >
+          <div className="CompanyTech-Text-Box-Title">
+            <strong>검출 한계</strong>
+          </div>
+          <div className="CompanyTech-Text-Box-Content">
+            *100 nano mol / Liter *100 nano mol/Liter는 소금물 100만 리터에
+            소금, 약 6g의 농도와 같습니다.
+          </div>
+        </div>
+        <div
+          className="CompanyTech-Text-Box"
+          data-aos="fade-up"
+          data-aos-delay="800"
+        >
+          <div className="CompanyTech-Text-Box-Title">
+            <strong>제품 타입</strong>
+          </div>
+          <div className="CompanyTech-Text-Box-Content">
+            분광 분석 기반의 물질 성분 분석 장비 혹은 분광 분석 모듈
+          </div>
+        </div>
       </div>
     </div>
   );
