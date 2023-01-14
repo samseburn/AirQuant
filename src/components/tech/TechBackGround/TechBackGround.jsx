3b574f0d9ac5af648178e7f70696bbfac5b9c688
Seeun <rlatp657@naver.com>
2023-01-14 18:36:18 +0900

    Feat: TecjBackGround 컴포넌트, 이미지 추가

---
+++ src/components/tech/TechBackGround/TechBackGround.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const TechBackGround = () => {
+  return (
+    <div className="TechBackGround">
+      <div className="TechBackGround_box">
+        <div className="TechBackGround_box_text">
+          <div className="TechBackGround_box_text_title">
+            <span className="TechBackGround_box_text_title_br">
+              좋은 공기는 
+            </span>
+            지구를 지키는 가장 핵심
+          </div>
+          <div className="TechBackGround_box_text_content">
+            PiQuant는 ESG 경영이나 에너지 효율화에 꼭 필요한 '분석' 분야를
+            선도하고 있으며, <br />
+            그린뉴딜 100대 혁신 기업에 선정되었습니다. <br />
+            우리는 미세먼지에 대한 세계적인 우려와 대기 오염 issue에 대응해{' '}
+            <br />
+            실내 공기 질 측정을 통해 환경 규제 및 가이드 라인에 맞추어 <br />
+            환경 개선을 위한 실시간 IoT 공기 질 모니터링 디바이스를
+            개발했습니다.
+            <br /> 또한, 혁신적인 기술력을 바탕으로 더 나은 세상을 만드는 것에
+            기여 중입니다.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechBackGround;
