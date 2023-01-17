0bd9cc2c0c93d9d6c62c22aa11add387f33a8558
Seeun <rlatp657@naver.com>
2023-01-17 09:43:50 +0900

    Docs: Tech 디자인 수정

--- src/components/tech/TechMiddleTitle/TechMiddleTitle.jsx
+++ src/components/tech/TechMiddleTitle/TechMiddleTitle.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import { middletitle_02 } from 'images';
 
 const TechMiddleTitle = () => {
-    return (
-        <div className='TechMiddleTitle__'>
-            <div className="TechMiddleTitle__container">
-
-                <div className='TechMiddleTitle__section'>
-                    <div className='TechMiddleTitle__text'>
-                    <div className='TechMiddleTitle__first'>
-                    미세먼지(PM10), 초미세먼지(PM2.5)를 포함한</div>
-                    <div className='TechMiddleTitle__second'>
-                    <span>총 11가지 </span>항목을 실시간으로 측정</div>
-                    </div>
-
-                    <img className="TechMiddleTitle__mainimage" src={middletitle_02} alt=""/>`
-                </div>
+  return (
+    <div className="TechMiddleTitle__">
+      <div className="TechMiddleTitle__container">
+        <div className="TechMiddleTitle__section">
+          <div className="TechMiddleTitle__text">
+            <div className="TechMiddleTitle__first">
+              미세먼지(PM10), 초미세먼지(PM2.5)를 포함한
             </div>
+            <div className="TechMiddleTitle__second">
+              <span>총 11가지 </span>항목을 실시간으로 측정
+            </div>
+          </div>
+
+          <img
+            className="TechMiddleTitle__mainimage"
+            src={middletitle_02}
+            alt=""
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TechMiddleTitle;
