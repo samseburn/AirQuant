8ac08374a09b7dfaa4776dc66323e23c0bf9ccc8
eunye <dms8945@gmail.com>
2023-01-18 16:13:49 +0900

    Docs: 버튼 컴포넌트 수정

--- src/components/module/Button/Button.jsx
+++ src/components/module/Button/Button.jsx
@@ -2,18 +2,11 @@ const Button = ({ url, text, data }) => {
   // props[functionName(sendData,openWindow),url,text] 받고
 
   const sendData = () => {
-    return alert(
-      `이메일: ${data.email} 
-   이름: ${data.name} 
-   제목: ${data.title} 
-   도움: ${data.help} 
-   문의: ${data.ask}`
-    );
+    console.log('==firebase to send data==');
   };
   const openWindow = () => {
     return window.open(`${url}`, '_blank');
   };
-
   return (
     <button
       className="Button"
